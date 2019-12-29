2c3dd0b6fd8d3a4b3a4fb4445b8e542f3066d8c9
ay8s <contact@andydev.co.uk>
2019-12-29 14:08:17 +0000

    Add Favicons etc

--- components/Layout.js
+++ components/Layout.js
@@ -9,8 +9,14 @@ const Layout = props => {
 	return (
     	<div className="content-wrapper">
 			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>				
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+				<link rel="manifest" href="/site.webmanifest"/>
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#eb000f"/>
+				<meta name="msapplication-TileColor" content="#000000"/>
+				<meta name="theme-color" content="#ffffff"/>
 			</Head>
 
 			<Header showOptions={props.showOptions} year={props.year} />
