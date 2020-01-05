1ea95a9048a65c6b9ac317844ac7af1a7ae31ca3
ay8s <contact@andydev.co.uk>
2020-01-05 13:45:41 -0800

    Add TBC Flag

--- components/Race.js
+++ components/Race.js
@@ -28,10 +28,13 @@ class Race extends React.Component {
 			<tbody id={ this.props.item.slug } key={ this.props.item.slug } className={`${moment(this.props.item.sessions.race).isBefore() ? 'past' : 'future'} ${this.props.index % 2 === 0 ? 'even' : 'odd'}`}>			
 				<tr key={this.props.item.slug} className="race" onClick={() => this.handleRowClick()}>
 					<td className="icon-column">
-						<i className={`${this.state.collapsed ? 'fal fa-caret-right fa-xs' : 'fal fa-caret-down fa-xs'}`}></i>
+						<i className={`${this.state.collapsed ? 'fas fa-caret-right fa-xs' : 'fas fa-caret-down fa-xs'}`}></i>
 					</td>
 					<td className="event-column">
-						{ this.props.item.name }
+						{ this.props.item.tbc &&
+							<span className="tbc">TBC</span>
+						}
+						{ this.props.item.name } Grand Prix
 					</td>
 					<td className="date-column">{ moment(this.props.item.sessions.race).tz(this.props.timezone).format('D MMM') }</td>
 					<td className="time-column">{ moment(this.props.item.sessions.race).tz(this.props.timezone).format('HH:mm') }</td>
@@ -71,10 +74,12 @@ class Race extends React.Component {
 				<style jsx>{`
 				
 				td, th {
-				    padding:16px;
+				    padding:16px 0;
 			    }
 			    
 			    .icon-column {
+				    padding:0;
+				    text-align:center;
 					font-size:20px;
 					width:5%;
 					color:#fff;   
@@ -92,6 +97,19 @@ class Race extends React.Component {
 				    width:20%;
 			    }
 			    
+			    .tbc {
+				    border-radius: 3px 3px 3px 3px;
+					-moz-border-radius: 3px 3px 3px 3px;
+					-webkit-border-radius: 3px 3px 3px 3px;
+					font-size:9px;
+					font-weight:800;
+				    display:inline-block;
+				    background: yellow;
+				    color: #000;
+				    padding: 1px 3px;
+				    margin-right:12px;
+				    vertical-align:middle;
+			    }
 			    
 			    .race { cursor: pointer; }
 			    
