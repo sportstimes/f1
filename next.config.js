cc16f227809735548414a5cfeaa5b69d62ff72fe
ay8s <contact@andydev.co.uk>
2020-01-12 18:29:14 -0800

    Testing Offline

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
 const withFonts = require('next-fonts');
-module.exports = withFonts({
+const withOffline = require('next-offline');
+
+const nextConfig = {
   webpack(config, options) {
 	config.node = { net: 'empty' }
     return config;
-  }
-});
+  },
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = withOffline(withFonts(nextConfig));
