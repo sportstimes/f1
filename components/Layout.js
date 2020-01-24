5d12aae73d052eb7094150d9d99fc67292c65c22
ay8s <contact@andydev.co.uk>
2020-01-23 16:49:53 -0800

    Revert back to correct cookie name

--- components/Layout.js
+++ components/Layout.js
@@ -45,7 +45,7 @@ const Layout = props => {
 			<CookieConsent
 			    location="bottom"
 			    buttonText="Great!"
-			    cookieName="f1cal1"
+			    cookieName="f1cal"
 			    style={{ background: "#0E5143", zIndex:999999, padding:"5px 0" }}
 			    buttonStyle={{ color: "white", background:"#1a8b73", fontSize: "13px" }}
 			    expires={150}
