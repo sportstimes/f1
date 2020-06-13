978882d36c7b900fa92e7f818ac937ddf37918d6
ay8s <contact@andydev.co.uk>
2020-06-12 20:23:28 -0700

    Revert index.js updates

--- pages_/index.js
+++ pages_/index.js
@@ -1,20 +1,17 @@
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
 import Layout from '../components/Layout';
 import Races from '../components/Races';
-import {NextSeo} from 'next-seo';
-import Link from 'next/link';
+import { NextSeo } from 'next-seo';
 import RaceSchema from '../components/RaceSchema';
 import useTranslation from 'next-translate/useTranslation'
 
 const Index = (props) => {
-    const {t, lang} = useTranslation()
+    const { t, lang } = useTranslation()
     const title = t('common:title')
     const subtitle = t('common:subtitle')
 
     const currentYear = '2020';
-    const metaDescription = t('common:meta.description', {year: currentYear})
-    const metaKeywords = t('common:meta.keywords', {year: currentYear})
+    const metaDescription = t('common:meta.description', { year: currentYear })
+    const metaKeywords = t('common:meta.keywords', { year: currentYear })
 
     return (
         <>
@@ -23,11 +20,11 @@ const Index = (props) => {
                 description={metaDescription}
                 keywords={metaKeywords}
             />
-            <Layout showOptions='true' showCalendarExport='true' year={props.year}>
-                <Races year={props.year} races={props.races}/>
+            <Layout showOptions='true' showCalendarExport='true' year={ props.year }>
+                <Races year={ props.year } races={ props.races } />
                 {props.races && props.races.map((item, index) => {
-                    if (item.sessions) {
-                        return (<RaceSchema item={item} key={item.name}/>)
+                    if(item.sessions){
+                        return (<RaceSchema item={item} key={item.name} />)
                     }
                 })}
             </Layout>
@@ -35,24 +32,16 @@ const Index = (props) => {
     );
 }
 
-export default Index;
-
-export const getStaticPaths = async () => {
-    return ({
-        paths: [],
-        fallback: false,
-    })
-}
-
-export const getStaticProps = async ({params}) => {
+Index.getInitialProps = async ({query: {timezone}, res}) => {
     const currentYear = '2020';
 
-    const data = await import(`../db/` + currentYear + `.json`)
+    const data = await import(`../db/`+currentYear+`.json`)
 
     return {
-        props: {
-            year: currentYear,
-            races: data.races
-        }
+        year: currentYear,
+        races: data.races,
+        virtual: data.virtual
     }
-};
+}
+
+export default Index;
