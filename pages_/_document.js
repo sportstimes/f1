2ae276541218322ab9bc4301e9282ea120a0ba12
ay8s <contact@andydev.co.uk>
2020-06-15 22:39:27 -0700

    html lang

---
+++ pages_/_document.js
@@ -0,0 +1,16 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import documentLang from 'next-translate/documentLang'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang={documentLang(this.props)}>
+                <Head />
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
