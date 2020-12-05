02b146ab97f291ab2c1e2e3ecb4750c5b3cf8cdb
ay8s <contact@andydev.co.uk>
2020-12-05 14:06:44 -0800

    Support button

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -16,44 +16,60 @@ class Header extends React.Component {
 		return (
 			<div className="w-full bg-dark-green mb-4">
 				<header className="max-w-screen-lg mx-auto font-sans px-2 py-4">
-					<div className="md:flex md:justify-start mb-4">
-						<div
-							className="mx-auto md:m-0 md:w-auto mb-4 md:mb-0"
-							style={{width: "60px"}}
-						>
-							<Link href="/">
-								<a name={title}>
-									<Logo />
-								</a>
-							</Link>
+					<div className="md:justify-between md:flex">
+						<div className="md:flex md:justify-start mb-4">
+							<div
+								className="mx-auto md:m-0 md:w-auto mb-4 md:mb-0"
+								style={{width: "60px"}}
+							>
+								<Link href="/">
+									<a name={title}>
+										<Logo />
+									</a>
+								</Link>
+							</div>
+							<div className="mt-1 ml-0 md:ml-4 text-center md:text-left font-title uppercase">
+								<h1 className="mb-1 text-lg tracking-wider">
+									<Link href="/">
+										<a
+											name={title}
+											className="text-white hover:text-white font-bold text-lg"
+										>
+											{title}&nbsp;
+											{this.props.year && (
+												<span>{this.props.year}</span>
+											)}
+											{lang != "en" && (
+												<span> | {languageNames[lang]}</span>
+											)}
+										</a>
+									</Link>
+								</h1>
+								<h2 className="text-xs font-normal tracking-wider">
+									<Link href="/">
+										<a
+											name={title}
+											className="text-light-green hover:text-light-green"
+										>
+											{t("common:subtitle")}
+										</a>
+									</Link>
+								</h2>
+							</div>
 						</div>
-						<div className="mt-1 ml-0 md:ml-4 text-center md:text-left font-title uppercase">
-							<h1 className="mb-1 text-lg tracking-wider">
-								<Link href="/">
-									<a
-										name={title}
-										className="hover:text-white font-bold text-lg"
-									>
-										{title}&nbsp;
-										{this.props.year && (
-											<span>{this.props.year}</span>
-										)}
-										{lang != "en" && (
-											<span> | {languageNames[lang]}</span>
-										)}
-									</a>
-								</Link>
-							</h1>
-							<h2 className="text-xs font-normal tracking-wider">
-								<Link href="/">
-									<a
-										name={title}
-										className="text-light-green hover:text-light-green"
-									>
-										{t("common:subtitle")}
-									</a>
-								</Link>
-							</h2>
+						<div className="hidden md:inline-block">
+							<a
+								href="https://www.buymeacoffee.com/f1cal"
+								className="support-btn mt-3"
+							>
+								<img
+									src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+									width="15"
+									height="15"
+									className="mr-2"
+								/>
+								Support us, buy us a coffee
+							</a>
 						</div>
 					</div>
 					<div className="clear-both"></div>
