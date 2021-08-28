51fcdf76dc8b1c4bb5bb56fe0fc1dbc41751e411
ay8s <contact@andydev.co.uk>
2021-08-28 17:48:19 +0100

    Fix ignore deploy check #f1

--- build/ignore-deploy.js
+++ build/ignore-deploy.js
@@ -1,9 +1,8 @@
 const siteKey = process.env.NEXT_PUBLIC_SITE_KEY;
 const gitMessage = process.env.VERCEL_GIT_COMMIT_MESSAGE;
 
-
-if(gitMessage.contains('#')){
-	if(gitMessage.contains(siteKey)){
+if(gitMessage.includes('#')){
+	if(gitMessage.includes(siteKey)){
 		return true;
 	} else {
 		return false;
