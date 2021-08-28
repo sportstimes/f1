e59d9976eb655954dbaf3b6842369d49a7ba2709
ay8s <contact@andydev.co.uk>
2021-08-28 16:22:25 +0100

    Test

--- build/ignore-delloy.js
+++ build/ignore-delloy.js
@@ -1,6 +1,7 @@
 const siteKey = process.env.NEXT_PUBLIC_SITE_KEY;
 const gitMessage = process.env.VERCEL_GIT_COMMIT_MESSAGE;
 
+
 if(gitMessage.contains('#')){
 	if(gitMessage.contains(siteKey)){
 		return true;
