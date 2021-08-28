70a687bf9eae3c2e74fc45ecf990f639413e1a3b
ay8s <contact@andydev.co.uk>
2021-08-28 17:52:23 +0100

    Add branch rule #f1

--- build/ignore-deploy.js
+++ build/ignore-deploy.js
@@ -1,7 +1,14 @@
 const siteKey = process.env.NEXT_PUBLIC_SITE_KEY;
+const gitBranch = process.env.VERCEL_GIT_COMMIT_REF;
 const gitMessage = process.env.VERCEL_GIT_COMMIT_MESSAGE;
 
-if(gitMessage.includes('#')){
+console.log(siteKey);
+console.log(gitBranch);
+console.log(gitMessage);
+
+if(gitBranch.includes(siteKey)){
+	return true;
+} else if(gitMessage.includes('#')){
 	if(gitMessage.includes(siteKey)){
 		return true;
 	} else {
