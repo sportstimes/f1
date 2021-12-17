9e9a914b99b0bb471aecfcd6220a758460fe44bc
ay8s <contact@andydev.co.uk>
2021-12-16 18:34:44 -0800

    Tidying up

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -18,30 +18,11 @@ export default function Footer() {
 					<div className="mt-1 mb-6 flex justify-center space-x-6">
 						<SiteSelector id="siteSelector2" />
 					</div>
-
-					<div className="mb-10 flex justify-center space-x-6">
-						<a
-							href="https://www.buymeacoffee.com/f1cal"
-							className="support-btn mt-3"
-							onClick={() =>
-								plausible("Support", {
-									props: {
-										buttonId: "footer"
-									}
-								})
-							}
-						>
-							<img
-								src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-								width="15"
-								height="15"
-								className="mr-2"
-								alt="buymeacoffee"
-							/>
-							{t("localization:footer.coffee")}
-						</a>
-					</div>
-
+	
+					<p className="mb-10 text-center text-base text-gray-400">
+						{ t('localization:footer.links.spottedIssue') } <a href="https://twitter.com/intent/tweet?text=%40f1cal%20I%20spotted%20an%20issue..." className="text-gray-300">{ t('localization:footer.links.spottedReport') }	</a>
+					</p>
+					
 					<div className="mb-8 flex justify-center space-x-6">
 						<a
 							href="https://twitter.com/f1cal"
@@ -57,7 +38,7 @@ export default function Footer() {
 								<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
 							</svg>
 						</a>
-
+					
 						<a
 							href="http://github.com/sportstimes/f1"
 							className="text-gray-400 hover:text-gray-500"
@@ -77,11 +58,6 @@ export default function Footer() {
 							</svg>
 						</a>
 					</div>
-
-					
-					<p className="mb-10 text-center text-base text-gray-400">
-						{ t('localization:footer.links.spottedIssue') } <a href="https://twitter.com/intent/tweet?text=%40f1cal%20I%20spotted%20an%20issue..." className="text-gray-300">{ t('localization:footer.links.spottedReport') }	</a>
-					</p>
 
 					<p className="mt-8 text-center text-base text-gray-400">
 						&copy;{" "}
