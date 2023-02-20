807b62211a1dc20268127acd610e33b6be836447
ay8s <contact@andydev.co.uk>
2023-02-20 13:15:10 -0800

    Update unsubscribe to use Novu

--- pages/api/email/unsubscribe/[subscriberUUID].js
+++ pages/api/email/unsubscribe/[subscriberUUID].js
@@ -11,69 +11,19 @@ export default async (req, res) => {
 	const config = await import(`../../../../_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`)  
 	
 	let subscriberUUID = req.query.subscriberUUID;
-		
-	const token = `${process.env.NEXT_PUBLIC_LISTMONK_USERNAME}:${process.env.NEXT_PUBLIC_LISTMONK_PASSWORD}`;
-	const encodedToken = Buffer.from(token).toString('base64');
 	
-	// Get the subscriber based on the UUID
-	const response = await fetch(`${process.env.NEXT_PUBLIC_LISTMONK_URL}/api/subscribers?query=subscribers.uuid%3D%27${subscriberUUID}%27`, {
-	  method: 'GET',
+	const response = await fetch('https://api.novu.co/v1/topics/reminder/subscribers/removal', {
+	  method: 'POST',
 	  headers: {
 		'Content-Type': 'application/json',
-		Authorization: 'Basic ' + encodedToken
-	  }
-	})
+		'Authorization': `ApiKey ${process.env.NEXT_PUBLIC_NOVU_API}`,
+	  },
+	  body: JSON.stringify({
+		subscribers: [subscriberUUID]
+	  }),
+	});
 	
-	const results = await response.json()
-	
-	if(results.data == null){
-		return res.status(400).json({success:false});
-	} else if(results.data.results.length != 0){
-		const subscriber = results.data.results[0];
+	const data = await response.json();
 		
-		// Create an array of lists the subscriber should remain subscribed to.
-		var listIDs = [];
-		var lists = subscriber.lists;
-		lists.forEach((list) => {
-			if(list.id != process.env.NEXT_PUBLIC_LISTMONK_LIST_ID){
-				listIDs.push(list.id);
-			}
-		});
-		
-		if(listIDs.length == 0){
-			// Remove the subscriber entirely...
-			const deleteResponse = await fetch(`${process.env.NEXT_PUBLIC_LISTMONK_URL}/api/subscribers/${subscriber.id}`, {
-			  method: 'DELETE',
-			  headers: {
-				'Content-Type': 'application/json',
-				Authorization: 'Basic ' + encodedToken
-			  }
-			})
-			
-			return res.status(200).json(deleteResponse);
-		} else {
-			// Update the subscriber...
-			let body = {
-				id:subscriber.id,
-				email:subscriber.email,
-				name:subscriber.name,
-				status:subscriber.status,
-				attribs:subscriber.attribs,
-				lists:listIDs
-			};
-			
-			const updateResponse = await fetch(`${process.env.NEXT_PUBLIC_LISTMONK_URL}/api/subscribers/${subscriber.id}`, {
-			  method: 'PUT',
-			  headers: {
-				'Content-Type': 'application/json',
-				Authorization: 'Basic ' + encodedToken
-			  },
-			  body: JSON.stringify(body)
-			});
-			
-			return res.status(200).json(updateResponse);
-		}
-	} else {
-		return res.status(400).json({success:false});
-	}
+	return res.status(200).json({success:true});
 }
