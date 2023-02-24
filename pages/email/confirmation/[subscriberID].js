b1015f8866ecae189e35b62e8ec897ef5c8fbf8b
ay8s <contact@andydev.co.uk>
2023-02-23 18:10:16 -0800

    Add config

--- pages/email/confirmation/[subscriberID].js
+++ pages/email/confirmation/[subscriberID].js
@@ -5,30 +5,32 @@ import Link from "next/link";
 import { useRouter } from 'next/router'
 
 const Confirmation = (props) => {
-	return (<>
+	return (
 		<Layout>
 			<h3 className="text-xl mb-4">Awesome!</h3>
 			<Card>
 				<p>Your email has been confirmed.</p>
 			</Card>
 		</Layout>
-	</>);
+	);
 };
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`https://f1calendar.com/api/email/confirmation/${context.query.subscriberID}`, {
-	headers: {
-	  'Content-Type': 'application/json'
-	}
-  });
-  
-  const result = await res.json()
-
-  return {
-	props: {
-	  result,
-	},
-  };
+	const config = require(`../_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`)
+	
+	const res = await fetch(`https://${config.url}/api/email/confirmation/${context.query.subscriberID}`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	
+	const result = await res.json()
+	
+	return {
+		props: {
+			result,
+		},
+	};
 };
 
 export default Confirmation;
--- pages/email/unsubscribe/[subscriberID].js
+++ pages/email/unsubscribe/[subscriberID].js
@@ -5,30 +5,32 @@ import Link from "next/link";
 import { useRouter } from 'next/router'
 
 const Unsubscribe = (props) => {
-	return (<>
+	return (
 		<Layout>
-			<h3 className="text-xl mb-4">Awesome!</h3>
+			<h3 className="text-xl mb-4">Sorry to see you go!</h3>
 			<Card>
 				<p>You have been unsubscribed.</p>
 			</Card>
 		</Layout>
-	</>);
+	);
 };
 
-export async function getServerSideProps(context) {
-  const res = await fetch(`https://f1calendar.com/api/email/unsubscribe/${context.query.subscriberID}`, {
-	headers: {
-	  'Content-Type': 'application/json'
-	}
-  });
-  
-  const result = await res.json()
-
-  return {
-	props: {
-	  result,
-	},
-  };
+export async function getServerSideProps(context) {	
+	const config = require(`../_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`)
+	
+	const res = await fetch(`https://${config.url}/api/email/unsubscribe/${context.query.subscriberID}`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	
+	const result = await res.json()
+	
+	return {
+		props: {
+			result,
+		},
+	};
 };
 
 export default Unsubscribe;
