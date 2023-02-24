6778cad231c1529ceb01753106b25d5525d5aa17
ay8s <contact@andydev.co.uk>
2023-02-23 18:29:12 -0800

    Adjsut CTAs based on whether the PWA is installed

--- components/CTABar/CTABar.tsx
+++ components/CTABar/CTABar.tsx
@@ -17,34 +17,41 @@ class CTABar extends React.Component<Props> {
 	constructor(props) {
 		super(props);
 		this.state = {
-		  supportsWebPush: false
+		  supportsWebPush: false,
+		  isInstalled: false
 		};
 	}
 	
 	componentDidMount() {
-		// TODO: Determine if browser supports web push
+		var installed = false;
+		if(window.navigator.standalone) {
+			installed = true;
+		}
+		
+		if(window.matchMedia('(display-mode: standalone)').matches){
+			installed = true;
+		}
+
 		if ('Notification' in window) {
-			this.setState({ supportsWebPush: true })
+			this.setState({ supportsWebPush: true, isInstalled:installed })
 		}
 	}
 	
 	render() {
 		const { t, lang } = this.props.i18n
 		
-		let gridCTAs = 1;
-		if(config.supportsEmailReminders) gridCTAs += 1;
-		if(config.supportsWebPush && this.state.supportsWebPush) gridCTAs += 1;
-		
 		return (
 			<div className={`grid grid-col-1 md:flex pt-4 gap-3 gap-y-2`}>
-				<div className="h-12 grow">
-					<Link href="/generate" className="bg-mid-green rounded-md shadow hover:bg-light-green hover:text-white flex justify-start content-center h-12 py-3 pl-12 relative">
-						{t("localization:options.calendar")}
-						<CalendarIcon className="absolute left-3 self-center" />
-						<ChevronRightIcon className="absolute right-3 top-4" />
-					</Link>
-				</div>
-				{config.supportsEmailReminders > 0 &&
+				{!this.state.isInstalled &&
+					<div className="h-12 grow">
+						<Link href="/generate" className="bg-mid-green rounded-md shadow hover:bg-light-green hover:text-white flex justify-start content-center h-12 py-3 pl-12 relative">
+							{t("localization:options.calendar")}
+							<CalendarIcon className="absolute left-3 self-center" />
+							<ChevronRightIcon className="absolute right-3 top-4" />
+						</Link>
+					</div>
+				}
+				{!this.state.isInstalled && config.supportsEmailReminders > 0 &&
 					<div className="h-12 grow">
 						<Link href="/subscribe" className="bg-mid-green rounded-md shadow hover:bg-light-green hover:text-white flex justify-start content-center h-12 py-3 pl-12 relative">
 							{t("localization:options.email")}
@@ -53,7 +60,7 @@ class CTABar extends React.Component<Props> {
 						</Link>
 					</div>
 				}
-				{config.supportsWebPush > 0 && this.state.supportsWebPush &&
+				{config.supportsWebPush > 0 && this.state.supportsWebPush && !this.state.isInstalled &&
 					<div className="h-12">
 						<Link href="/notifications" className="bg-mid-green rounded-md shadow hover:bg-light-green hover:text-white flex justify-start content-center h-12 py-3 pl-12 relative" title={t("localization:options.notifications")}>
 							<NotificationIcon className="absolute left-3.5 self-center" />
@@ -62,6 +69,17 @@ class CTABar extends React.Component<Props> {
 						</Link>
 					</div>
 				}
+				
+				{config.supportsWebPush > 0 && this.state.supportsWebPush && this.state.isInstalled &&
+					<div className="h-12 grow">
+						<Link href="/notifications" className="bg-mid-green rounded-md shadow hover:bg-light-green hover:text-white flex justify-start content-center h-12 py-3 pl-12 relative" title={t("localization:options.notifications")}>
+							<NotificationIcon className="absolute left-3.5 self-center" />
+							{t("localization:options.notifications")}
+							<ChevronRightIcon className="absolute right-3 top-4" />
+						</Link>
+					</div>
+				}
+				
 			</div>
 		);
 	}
