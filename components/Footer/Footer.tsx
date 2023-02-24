479ec157eaa455fdf584c34428d928c65d2e2efb
ay8s <contact@andydev.co.uk>
2023-02-23 18:43:27 -0800

    Add email icon to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import LanguageSelector from "../../components/LanguageSelector/LanguageSelector
 import SiteSelector from "../../components/SiteSelector/SiteSelector";
 import {usePlausible} from "next-plausible";
 import type { I18n } from 'next-translate'
+import EmailIcon from '../Icons/EmailIcon'
 
 interface Props {
 	i18n: I18n;
@@ -136,6 +137,14 @@ class Footer extends React.Component<Props> {
 									/>
 								</svg>
 							</a>
+							
+							<a href="https://www.andydev.co.uk/contact" className="text-gray-400 hover:text-gray-500">
+								<span className="sr-only">Email</span>
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"
+								fill="currentColor" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+							</a>
+							
+							
 						</div>
 					</div>
 					
