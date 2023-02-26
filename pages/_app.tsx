0e301d5fdb5947940082fab1ed5c9adf0fac2750
ay8s <contact@andydev.co.uk>
2023-02-25 22:39:17 -0800

    Test remove showNotification within app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,7 +49,7 @@ export default function CalendarApp({ Component, pageProps }: AppProps) {
 					};
 				
 					navigator.serviceWorker.ready.then((registration) => {
-						registration.showNotification(title, options);
+						//registration.showNotification(title, options);
 					});
 				});
 			}
