316601e882f9b342a38d02e26e91a1259d0c725e
ay8s <contact@andydev.co.uk>
2023-02-27 22:22:58 -0800

    Refactoring

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -5,11 +5,6 @@ import { Novu } from '@novu/node';
 import { TriggerRecipientsTypeEnum } from '@novu/shared';
 
 export default async (req, res) => {
-	// if (req.query.key !== 'sharedKey') {
-	// 	res.status(404).end();
-	// 	return;
-	// }
-	
 	// Check if either email or push is supported.
 	const config = await import(`../../_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`)	
 	if(!config.supportsEmailReminders && !config.supportsWebPush){
@@ -17,30 +12,9 @@ export default async (req, res) => {
 		return;
 	}
 	
-	// Initialize Firebase
-	firebase.initializeApp({
-		apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-		authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-		storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
-		appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-	})
+	var isBetween = require('dayjs/plugin/isBetween')
+	dayjs.extend(isBetween)
 	
-	// Where we store the dates of the last send, used to protect against sending extra notifications etc.
-	const db = firebase.firestore();
-	const docRef = db.collection("sites").doc(process.env.NEXT_PUBLIC_SITE_KEY)
-	const docData = await docRef.get()
-	
-	// Initialize Novu
-	const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API);
-	
-	// Get the English localization file...
-	const localization = await import(`../../locales/en/localization.json`)
-	const localizationSite = localization[process.env.NEXT_PUBLIC_SITE_KEY];
-	const localizationSchedule = localization.schedule;
-	const localizationRaces = localization.races;
-
 	// Get this years calendar
 	const data = await import(`../../_db/${process.env.NEXT_PUBLIC_SITE_KEY}/${process.env.NEXT_PUBLIC_CURRENT_YEAR}.json`)
 	const races = data.races;
@@ -59,7 +33,6 @@ export default async (req, res) => {
 		}
 	});
 
-
 	// Figure out the first and last sessions.
 	let firstEventSessionKey = Object.keys(nextRace.sessions)[0];
 	let lastEventSessionKey = Object.keys(nextRace.sessions)[
@@ -69,58 +42,97 @@ export default async (req, res) => {
 	const firstSession = dayjs(nextRace.sessions[firstEventSessionKey]);
 	const lastSession = dayjs(nextRace.sessions[lastEventSessionKey]);
 	
-	// Notification Gap
-	var date = new Date();
-	date.setMinutes(date.getMinutes() + 5); 
 	
-	// Are we within a weekend?
-	if(firstSession.isBefore(date) && lastSession.isAfter(date)){
-		// Within a weekend. So lets check if we need to send a session reminder...		
-		var nextSession = null;
-		
-		Object.keys(nextRace.sessions).forEach(function (session, index) {
-			if (dayjs(nextRace.sessions[session]).isAfter(Date()) && nextSession == null) {
-				nextSession = session;
-			}
-		});
-		
-		const nextSessionDate = dayjs(nextRace.sessions[nextSession]);
-		
-		if(nextSessionDate.diff(date, 'minutes') <= 1){
-			// Ensure we're not about to send a duplicate trigger to Novu...
-			if (docData.exists && dayjs(Date()).diff(dayjs(docData.data()[nextSession]), 'minutes') > 60) {
-				console.log("Trigger Topic: pushReminder, " + nextSession);
-				
-				const sessionTopic = `${process.env.NEXT_PUBLIC_SITE_KEY}-${nextSession}`;
-				
-				var title = nextRace.name;
-				if(localizationRaces[nextRace['localeKey']]){
-					title = localizationRaces[nextRace['localeKey']];
-				}
-				
-				const body = `${localizationSchedule[nextSession]} starts shortly.`;
-				
-				await novu.trigger('pushreminder', {
-					to: [{ type: TriggerRecipientsTypeEnum.TOPIC, topicKey: sessionTopic }],
-					payload: {
-						title:title,
-						content:body
-					},
-				});
-				
-				await docRef.set({[nextSession]:Date()}, { merge: true });
-			} else {
-				console.log("Already sent! " + nextSession);
-			}
-		} else {
-			console.log("Not within 5 minutes of a session");
-		}
+	// We send emails out an hour ahead.
+	const hourBeforeFirstSession = firstSession.subtract(1, 'hour');
+	const dateNow = dayjs();
+	
+	// Are we within a race weekend - 1 hour?
+	if(!dateNow.isBetween(hourBeforeFirstSession, lastSession, 'minute')){
+		res.json({ success: true, message:'Not within a race weekend' });
+		return;
 	}
 	
-	if(firstSession.diff(Date(), 'minutes') < 60 && firstSession.diff(Date(), 'minutes') > 57){
+	
+	// Now we know we need to continue, init Firebase and Novu etc
+	// Get the English localization file...
+	const localization = await import(`../../locales/en/localization.json`)
+	const localizationSite = localization[process.env.NEXT_PUBLIC_SITE_KEY];
+	const localizationSchedule = localization.schedule;
+	const localizationRaces = localization.races;
+	
+	
+	// Initialize Firebase
+	firebase.initializeApp({
+		apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+		authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+		storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
+		appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+	})
+	
+	
+	// Where we store the dates of the last send, used to protect against sending extra notifications etc.
+	const db = firebase.firestore();
+	const docRef = db.collection("sites").doc(process.env.NEXT_PUBLIC_SITE_KEY)
+	const docData = await docRef.get()
+	
+	const debugRef = db.collection("debug").doc(process.env.NEXT_PUBLIC_SITE_KEY)
+	const emailDebugRef = db.collection("emailDebug").doc(process.env.NEXT_PUBLIC_SITE_KEY)
+	
+	// Initialize Novu
+	const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API);
+	
+	// Within a weekend. So lets check if we need to send a session reminder...		
+	var nextSession = null;
+	
+	Object.keys(nextRace.sessions).forEach(function (session, index) {
+		if (dayjs(nextRace.sessions[session]).isAfter(Date()) && nextSession == null) {
+			nextSession = session;
+		}
+	});
+	
+	const nextSessionDate = dayjs(nextRace.sessions[nextSession]);
+	const nextSessionDateMinus5 = nextSessionDate.subtract(5, 'minutes');
+
+	if(dateNow.isBetween(nextSessionDateMinus5, nextSessionDate, 'minute')){
+		// Ensure we're not about to send a duplicate trigger to Novu...
+		if (docData.exists && dayjs(Date()).diff(dayjs(docData.data()[nextSession]), 'minutes') > 60) {
+			console.log("Trigger Topic: pushReminder, " + nextSession);
+			
+			const sessionTopic = `${process.env.NEXT_PUBLIC_SITE_KEY}-${nextSession}`;
+			
+			var title = nextRace.name;
+			if(localizationRaces[nextRace['localeKey']]){
+				title = localizationRaces[nextRace['localeKey']];
+			}
+			
+			const body = `${localizationSchedule[nextSession]} starts soon.`;
+			
+			await novu.trigger('pushreminder', {
+				to: [{ type: TriggerRecipientsTypeEnum.TOPIC, topicKey: sessionTopic }],
+				payload: {
+					title:title,
+					content:body
+				},
+			});
+			
+			await docRef.set({[nextSession]:Date()}, { merge: true });
+			
+			await debugRef.set({[Date()]:nextSession}, { merge: true });
+		} else {
+			console.log("Already sent! " + nextSession);
+		}
+		
+	}
+	
+	let emailCuttoff = hourBeforeFirstSession.subtract(2, 'minutes');
+	if(dateNow.isBetween(emailCuttoff, hourBeforeFirstSession, 'minute')){
 		// Ensure we're not about to send a duplicate trigger to Novu...
 		// Did we send an email within the last hour?
-		if (docData.exists && dayjs(Date()).diff(dayjs(docData.data()['email-reminder']), 'minutes') > 60) {
+		
+		if (docData.exists && dayjs(Date()).diff(dayjs(docData.data()['email-reminder']), 'minutes') > 90) {
 			const reminderTopic = `${process.env.NEXT_PUBLIC_SITE_KEY}-reminder`;
 
 			console.log("Trigger Topic: email-weekend, " + JSON.stringify(nextRace));
@@ -138,8 +150,8 @@ export default async (req, res) => {
 			});
 			
 			await docRef.set({'email-reminder':Date()}, { merge: true });
-		} else {
-			console.log("Already sent race-weekend!");
+			
+			await emailDebugRef.set({[Date()]:nextSession}, { merge: true });
 		}
 	}
 
