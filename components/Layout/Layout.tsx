c1835ee85c149466decfb6c2d2f8b8bff87c4a39
ay8s <contact@andydev.co.uk>
2023-03-03 14:37:15 -0800

    Remove code duplication and move to _document

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent, ReactChildren, ReactChild, PropsWithChildren } from "react";
-import Head from "next/head";
 import useTranslation from 'next-translate/useTranslation'
 import TopBar from "../../components/TopBar/TopBar";
 import Header from "../../components/Header/Header";
@@ -13,60 +12,9 @@ type Props = PropsWithChildren<{
 
 const Layout: FunctionComponent<Props> = ({ showCTABar, children, year }: Props) => {
 	const { t, lang } = useTranslation();
-	const title = t(`localization:` + process.env.NEXT_PUBLIC_SITE_KEY + `.title`);
 
 	return (
 		<>
-			<Head>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, maximum-scale=5"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-				<link
-					rel="mask-icon"
-					href="/safari-pinned-tab.svg"
-					color="#eb000f"
-				/>
-				
-				<meta name="msapplication-TileColor" content="#000000" />
-				<meta name="theme-color" content="#03120f" />
-				
-				<meta name="apple-mobile-web-app-capable" content="yes" />
-				<meta name="apple-mobile-web-app-status-bar-style" content="default" />
-				<meta name="apple-mobile-web-app-title" content={title} />
-				<meta name="format-detection" content="telephone=no" />
-				<meta name="mobile-web-app-capable" content="yes" />
-				
-				<link rel="preconnect" href="https://fonts.googleapis.com" /> 
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> 
-				<link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@700;800&display=swap" rel="stylesheet" />
-
-				{process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION && (
-					<meta
-						name="google-site-verification"
-						content={process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION}
-					/>
-				)}
-			</Head>
-
 			<noscript>
 				<div className="noscript">{t("localization:javascript")}</div>
 			</noscript>
