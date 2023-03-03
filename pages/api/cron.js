b15aca616adad8796cb1b423401be448337b0298
ay8s <contact@andydev.co.uk>
2023-03-02 17:30:36 -0800

    Improve cron

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -11,10 +11,13 @@ export default async (req, res) => {
 	}
 	const db = admin.firestore()
 	
-	// Grab any documents which have a scheduledAt date in the past, which means we should send them...
-	const docRef = db.collection(process.env.NEXT_PUBLIC_SITE_KEY).where('scheduledAt', '<', new Date())
-	const docSnapshot = await docRef.get()
+	const cutoffDate = dayjs(Date()).subtract(3, 'minutes');
 	
+	// Grab any documents which have a scheduledAt date in the past, which means we should send them...
+	// But only if they're not older than 3 minutes, if they are something has gone wrong and there not sending
+	
+	const docRef = db.collection(process.env.NEXT_PUBLIC_SITE_KEY).where('scheduledAt', '<', new Date()).where('scheduledAt', '>', cutoffDate.toDate())
+	const docSnapshot = await docRef.get()
 	
 	if (docSnapshot.empty) {
 		console.log("Nothing to send");
@@ -32,24 +35,37 @@ export default async (req, res) => {
 		const body = scheduledItem.body;
 		
 		if(scheduledItem.type == "push"){
-			const novuResponse = await novu.trigger('pushreminder', {
-				to: [{ type: TriggerRecipientsTypeEnum.TOPIC, topicKey: scheduledItem.topic }],
-				payload: {
-					title:title,
-					content:body
-				},
-			});
-			
-			// Remove the document so we don't send it again!	
-			await item.ref.delete();
+			try {
+				const novuResponse = await novu.trigger('pushreminder', {
+					to: [{ type: TriggerRecipientsTypeEnum.TOPIC, topicKey: scheduledItem.topic }],
+					payload: {
+						title:title,
+						content:body
+					},
+				});
+				
+				console.log("push")
+				console.log(novuResponse)
+				
+				// Remove the document so we don't send it again!	
+				await item.ref.delete();
+			} catch(error){
+				console.log("pusherror "+ error)
+			}
 		} else if(scheduledItem.type == "email"){
-			const emailResponse = await novu.trigger('emailreminder', {
-				to: [{ type: TriggerRecipientsTypeEnum.TOPIC, topicKey: scheduledItem.topic }],
-				payload: {title: title},
-			});
-		
-			// Remove the document so we don't send it again!	
-			await item.ref.delete();
+			try {
+				const emailResponse = await novu.trigger('emailreminder', {
+					to: [{ type: TriggerRecipientsTypeEnum.TOPIC, topicKey: scheduledItem.topic }],
+					payload: {title: title},
+				});
+				console.log("emailResponse:")
+				console.log(emailResponse)
+				
+				// Remove the document so we don't send it again!	
+				await item.ref.delete();
+			} catch(error){
+				console.log("email error:"+ error)
+			}
 		} else if(scheduledItem.type == "buffer"){
 			const response = await fetch(`https://api.bufferapp.com/1/updates/create.json?access_token=${encodeURI(process.env.NEXT_PUBLIC_BUFFER_TOKEN)}`, {
 			  method: 'POST',
@@ -57,6 +73,8 @@ export default async (req, res) => {
 			  body: `text=${encodeURIComponent(scheduledItem.title)}&profile_ids[]=63ff8ab4a439208bacf17292&profile_ids[]=63ff8acfa439208bacf26e2a`,
 			});
 			const data = await response.json();
+			
+			console.log("Buffer"+ JSON.stringify(data))
 			
 			// Remove the document so we don't send it again!	
 			await item.ref.delete();
