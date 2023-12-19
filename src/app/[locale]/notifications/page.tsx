478ebbfa0f3e4e740c787893b9168bb1c362737f
ay8s <contact@andydev.co.uk>
2023-12-19 14:55:34 -0800

    Shifting forms to own components

--- src/app/[locale]/notifications/page.tsx
+++ src/app/[locale]/notifications/page.tsx
@@ -1,12 +1,21 @@
-"use client"
-
-import React, { useState, useCallback, useEffect } from "react";
+import {useTranslations} from 'next-intl';
+import {getTranslations} from 'next-intl/server';
 import Layout from "components/Layout/Layout";
 import Card from "components/Card/Card";
 import {usePlausible} from "next-plausible";
-import {useTranslations} from 'next-intl';
-import {firebaseCloudMessaging} from "../../../../config/firebase";
 import { notFound } from 'next/navigation'
+import Form from "./form"
+
+export async function generateMetadata()  {
+  const t = await getTranslations('All');
+  const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR;
+
+  return {
+    title: `${t('notifications.title')} - ${t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.title`, {year: currentYear})}`,
+    description: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.description`, {year: currentYear}),
+    keywords: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.keywords`, {year: currentYear}),
+  }
+}
 
 export default function Notifications() {
   const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR;
@@ -14,210 +23,6 @@ export default function Notifications() {
   const t = useTranslations('All');
   
   const config = require(`/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`);
-  
-  // Sessions
-  var sessions = config.sessions;
-  
-  // Default form values...
-  var defaults = {
-    submitted: false
-  };
-  
-  // Remove Sprint Qualifying
-  sessions = sessions.filter(item => item !== "sprintQualifying");
-  
-  // Add sessions from config...
-  sessions.forEach(function (session:String, index:Number) {
-    defaults[session] = false;
-  });
-  
-  const [form, setState] = useState(defaults);
-  
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-  
-    const sessions = config.sessions;
-    const sessionMap = config.sessionMap;
-    let topics = [];
-  
-    sessions.forEach(function (session, index) {
-      if (form[session]) {
-        topics.push(session);
-      }
-    });
-    
-    setState({
-      ...form,
-      submitted: true,
-    });
-    
-    const res = await fetch('/api/notifications/update', {
-      body: JSON.stringify({
-      identifier: localStorage.getItem("uuid"),
-      topics: topics
-      }),
-      headers: {
-      'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    });
-    
-    const result = await res.json()
-    
-    setState({
-      ...form,
-      saved: true,
-    });
-    
-    plausible("Saved Notification Settings");
-  };
-  
-  const [permission, setPermission] = useState<"denied" | "default" | "granted">("denied")
-    const [fcmToken, setFcmToken] = useState<string|undefined>(undefined);
-  const [loaded, setLoaded] = useState<Boolean>(false)
-  
-  const getToken = async () => {
-    try {
-      const token = await firebaseCloudMessaging.init()
-      if (token) {
-        
-        const subscribeRes = await fetch('/api/notifications/subscribe', {
-          body: JSON.stringify({
-          identifier: localStorage.getItem("uuid"),
-          token: token
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          method: 'POST'
-        });
-        
-        setFcmToken(token)
-        
-        await getSubscriptions();
-      } else {
-        console.log("No token?!?");
-      }
-    } catch (error) {
-      console.log("err1:" +error)
-    }
-  }
-  
-  const getSubscriptions = async () => {
-    try {
-      const res = await fetch(`/api/notifications/subscriptions?identifier=${localStorage.getItem("uuid")}`);
-      const result = await res.json()
-      const subscriptions = result.subscriptions
-      
-      setState(subscriptions);
-      setLoaded(true);
-    } catch (error) {
-      console.log("err2:" + error)
-    }
-  }
-  
-  const checkNotification = useCallback(async () => {
-    const status = Notification.permission;
-    setPermission(status)
-    
-    if(status === 'granted') {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then((registration) => {
-          getToken();
-        });
-      }
-    }
-  }, [getToken])
-  
-  useEffect(() => {
-    if ('permissions' in navigator) {
-      navigator.permissions.query({ name: 'notifications' }).then(function (notificationPerm) {
-        notificationPerm.onchange = async function () {
-          await checkNotification()
-        };
-      });
-    }
-    async function initialize() {
-      await checkNotification()
-    }
-    
-    initialize()
-  }, [])
-  
-  const delay = time => new Promise(res=>setTimeout(res,time));
-  
-  const handleRequestPermission = async () => {
-    plausible("Enabled Notifications");
-    
-    await Notification.requestPermission();
-    checkNotification()
-  }
-  
-  const renderDeniedNotificationBlock = () => (
-    <p>
-      {t("notifications.permissions.denied")}
-    </p>
-  )
-  
-  const renderAllowNotificationBlock = () => (
-    <>
-      <p className="mb-4">{t("notifications.permissions.prompt")}</p>
-      <p>
-        <button type="button" className="btn" onClick={handleRequestPermission}>
-          {t("notifications.permissions.button")}
-        </button>	
-      </p>
-    </>
-  )
-  
-  const renderGrantedNotificationBlock = () => (
-    <>
-      <p className="mb-4">{t("notifications.description")}</p>
-      <p className="mb-8">{t("notifications.statement")}</p>
-      
-      <form id="download_subscribe" onSubmit={handleOnSubmit}>
-        <fieldset className="mb-6" key="options">
-          {sessions.map((item, index) => {
-            let defaultValue = form[item] ? "on" : "off";
-            let defaultChecked = form[item] ? "checked" : "";
-            
-            return (
-              <div className="mb-4" key={item}>
-                <input
-                  type="checkbox"
-                  className="form-tick mr-3 bg-white appearance-none checked:bg-light-green checked:border-transparent w-6 h-6 rounded-md border inline-block align-middle"
-                  name={item}
-                  id={item}
-                  checked={defaultChecked}
-                  onChange={async event => {
-                    setState({
-                      ...form,
-                      [item]: event.target.checked
-                    })
-                  }}
-                />
-                <label
-                  htmlFor={item}
-                  className="inline-block align-middle text-base"
-                >
-                  {t(`schedule.${item}`)}
-                </label>
-              </div>
-            );
-          })}
-        </fieldset>
-      
-        <fieldset id="buttons" key="buttons" className="flex gap-x-4">
-          <button type="submit" className="btn">
-            {!form.submitted
-              ? t("notifications.button")
-              : t("notifications.buttonSubmitted")}
-          </button>
-        </fieldset>
-      </form>
-    </>
-  )
-  
   
   if(!config.supportsWebPush){
     notFound()
@@ -229,15 +34,7 @@ export default function Notifications() {
         {t("notifications.title")} (Beta)
       </h3>
       <Card>
-        {permission === "denied" && renderDeniedNotificationBlock()}
-    
-        {permission === "default" && renderAllowNotificationBlock()}
-    
-        {permission === "granted" && loaded && renderGrantedNotificationBlock()}
-        
-        {permission === "granted" && !loaded && (
-          <p>Loading</p>
-        )}
+        <Form />
       </Card>
     </Layout>
   )
--- src/app/[locale]/subscribe/page.tsx
+++ src/app/[locale]/subscribe/page.tsx
@@ -1,11 +1,22 @@
-"use client"
-
-import React, { useState } from "react";
+import {useTranslations} from 'next-intl';
+import {getTranslations} from 'next-intl/server';
+import React from "react";
 import Layout from "components/Layout/Layout";
 import Card from "components/Card/Card";
 import {usePlausible} from "next-plausible";
-import {useTranslations} from 'next-intl';
 import { notFound } from 'next/navigation'
+import Form from "./form"
+
+export async function generateMetadata()  {
+  const t = await getTranslations('All');
+  const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR;
+
+  return {
+    title: `${t('subscribe.title')} - ${t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.title`, {year: currentYear})}`,
+    description: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.description`, {year: currentYear}),
+    keywords: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.keywords`, {year: currentYear}),
+  }
+}
 
 export default function Subscribe() {
   
@@ -14,9 +25,6 @@ export default function Subscribe() {
   const t = useTranslations('All');
   
   const config = require(`/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`);
-  
-  const [showSuccess, setShowSuccess] = useState(false);
-  const [showSubmitting, setShowSubmitting] = useState(false);
   
   if(!config.supportsEmailReminders){
     notFound()
@@ -27,87 +35,7 @@ export default function Subscribe() {
       <h3 className="text-xl mb-4">{t("subscribe.title")}</h3>
       <Card>
         <p className="pl-px pb-5">{t("subscribe.description")}</p>
-        <form
-          action="https://f1calendar.us10.list-manage.com/subscribe/post?u=e11245c4d3fecdad90cb66908&amp;id=f7a8a5001f"
-          method="post"
-          id="subscribe-form"
-          name="mc-embedded-subscribe-form"
-          className="validate"
-          target="_blank"
-          noValidate
-          style={{ display: showSuccess ? "none":"block" }}
-          onSubmit={async event => {
-            event.preventDefault();
-            
-            var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-      
-            if (!emailRegex.test(event.target.EMAIL.value)){
-              alert("You have entered an invalid email address!")
-              return;
-            }
-            
-            setShowSubmitting(true)
-            
-            const res = await fetch('/api/email/subscribe', {
-              body: JSON.stringify({
-              email: event.target.EMAIL.value,
-              identifier: localStorage.getItem('uuid'),
-              }),
-              headers: {
-              'Content-Type': 'application/json'
-              },
-              method: 'POST'
-            });
-            
-            const result = await res.json()
-            
-            setShowSuccess(true)
-            
-            plausible("Subscribed to Email Alerts");
-          }}
-        >
-          <div className="row">
-            <label htmlFor="mce-EMAIL" className="font-bold text-lg">
-              {t("subscribe.label")}
-            </label>
-            <input
-              type="email"
-              name="EMAIL"
-              className="required px-2 email w-full py-2 text-black bg-gray-100 shadow-inner rounded-md border border-gray-400 focus:outline-none mt-2 mb-4"
-              id="mce-EMAIL"
-              placeholder="your@email.com"
-            />
-          </div>
-      
-          <div className="hidden">
-            <input
-              type="text"
-              name="b_e11245c4d3fecdad90cb66908_f7a8a5001f"
-              tabIndex="-1"
-              defaultValue=""
-            />
-          </div>
-      
-          <input
-            type="submit"
-            value={t("subscribe.button")}
-            name="subscribe"
-            id="mc-embedded-subscribe"
-            className="btn"
-            style={{ display: showSubmitting ? "none":"block" }}
-          />
-          
-          <input
-            type="submit"
-            value="..."
-            disabled="disabled"
-            name="subscribe"
-            id="mc-embedded-subscribe"
-            className="btn"
-            style={{ display: showSubmitting ? "block":"none" }}
-          />
-        </form>
-        <p style={{ display: showSuccess ? "block":"none" }}>Success!</p>
+        <Form />
       </Card>
     </Layout>
   );
