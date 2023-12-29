ad0be5c516cee574fa41a03dc403cf522b9ba6e9
ay8s <contact@andydev.co.uk>
2023-12-28 21:31:22 -0800

    Add missing imports

--- src/components/Badges/CanceledBadge.tsx
+++ src/components/Badges/CanceledBadge.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react"
+import {useTranslations} from 'next-intl';
 
 interface Props {
 	mobileOnly?: boolean;
--- src/components/Badges/TicketsBadge.tsx
+++ src/components/Badges/TicketsBadge.tsx
@@ -1,17 +1,18 @@
 import React, {useState} from "react"
+import {useTranslations} from 'next-intl';
 
 interface Props {
 	i18n: I18n;
 }
 
-class TicketsBadge extends React.Component<Props> {
-  render() {
+const TicketsBadge: FunctionComponent<Props> = ({ mobileOnly }: Props) => {
+	const t = useTranslations('All');
+
 	return (
 		<span title={t('badges.tickets')} className={`bg-green-600 hover:bg-green-700 rounded px-1 md:px-2 py-1 text-xxs sm:text-xs text-black font-normal sm:font-bold ml-2`}>
 			{t('badges.tickets')}
 		</span>
 	);
-  }
 }
 
 export default TicketsBadge;
