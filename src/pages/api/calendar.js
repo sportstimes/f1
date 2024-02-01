17e68420f18cc3b5f1294bda32146a6ae49f92c0
ay8s <contact@andydev.co.uk>
2024-01-31 22:01:44 -0800

    Fix additional output again

--- src/pages/api/calendar.js
+++ src/pages/api/calendar.js
@@ -33,11 +33,10 @@ export default async (req, res) => {
       output.notice = config.notice;
     }
     
-    res.json(output)
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.setHeader('Cache-Control', 's-maxage=80000, stale-while-revalidate');
-    res.json(data["default"])
+    res.json(output)
   } catch (err) { 
     res.statusCode = 404
     res.setHeader('Content-Type', 'application/json')
