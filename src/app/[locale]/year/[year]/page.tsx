882cc75d4f4e89504da65030267b1c44f8d2e37a
ay8s <contact@andydev.co.uk>
2024-12-05 18:35:40 -0800

    Update Packages & add 2025

--- src/app/[locale]/year/[year]/page.tsx
+++ src/app/[locale]/year/[year]/page.tsx
@@ -1,63 +1,71 @@
-import {notFound} from 'next/navigation';
-import {useTranslations} from 'next-intl';
-import {getTranslations, unstable_setRequestLocale} from 'next-intl/server';
-import Layout from "components/Layout/Layout";
-import Card from "components/Card/Card";
-import Link from "next/link";
-import OptionsBar from "components/OptionsBar/OptionsBar";
-import Races from "components/Races/Races";
+import { notFound } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+import Layout from 'components/Layout/Layout';
+import Card from 'components/Card/Card';
+import Link from 'next/link';
+import OptionsBar from 'components/OptionsBar/OptionsBar';
+import Races from 'components/Races/Races';
 
 export interface Props {
-  params: { year: string, locale: string };
+  params: { year: string; locale: string };
 }
 
 export async function generateMetadata({
-  params: {year}
+  params: { year },
 }: Omit<Props, 'children'>): Promise<Metadata> {
   const t = await getTranslations('All');
 
   return {
-    title: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.title`, {year: year}),
-    description: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.description`, {year: year}),
-    keywords: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.keywords`, {year: year}),
-  }
+    title: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.title`, { year: year }),
+    description: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.description`, {
+      year: year,
+    }),
+    keywords: t(`${process.env.NEXT_PUBLIC_SITE_KEY}.seo.keywords`, {
+      year: year,
+    }),
+  };
 }
 
-export async function generateStaticParams() {  
-  const config = require(`/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`);
-  
+export async function generateStaticParams() {
+  const config = require(
+    `/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`,
+  );
+
   let availableYears = config.availableYears;
-  
+
   const yearItems = [];
-  for (let year in availableYears) {
-    yearItems.push(config.availableYears[year]);
-  }
-  
+  // for (let year in availableYears) {
+  //   yearItems.push(config.availableYears[year]);
+  // }
+
   return yearItems;
 }
 
-export default function Year({params}: Props) {
-  const config = require(`/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`);
-  
+export default function Year({ params }: Props) {
+  const config = require(
+    `/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`,
+  );
+
   let availableYears = config.availableYears;
-  if(!availableYears.includes(parseInt(params.year))){
+  if (!availableYears.includes(parseInt(params.year))) {
     notFound();
   }
-  
+
   unstable_setRequestLocale(params.locale);
 
   const t = useTranslations('All');
-  
+
   const year = params.year;
   const data = require(`/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/${year}.json`);
 
-  if(data.races){
+  if (data.races) {
     return (
       <Layout year={year}>
         <OptionsBar pickerShowing={false} />
         <Races year={year} races={data.races} />
       </Layout>
-    )
+    );
   } else {
     return (
       <Layout>
@@ -66,15 +74,12 @@ export default function Year({params}: Props) {
         </h3>
         <Card>
           <p>
-            Want to add more historical dates to F1 Calendar?
-            Contribute previous seasons via our{" "}
-            <a href="https://github.com/sportstimes/f1">
-              GitHub repository
-            </a>
-            .
+            Want to add more historical dates to F1 Calendar? Contribute
+            previous seasons via our{' '}
+            <a href="https://github.com/sportstimes/f1">GitHub repository</a>.
           </p>
         </Card>
       </Layout>
-    ) 
+    );
   }
 }
