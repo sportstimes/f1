8dbf39ba4df44af7fd049364624e22dd2e2a89da
ay8s <contact@andydev.co.uk>
2025-01-03 18:58:25 -0800

    Add timezone pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import type { MetadataRoute } from 'next';
 import i18nConfig from '../i18nConfig.js';
+import ct from 'countries-and-timezones';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const config = require(
@@ -69,6 +70,27 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 0.5,
       alternates: createAlternates('/subscribe'),
+    });
+  }
+
+  const timezoneItems = [];
+
+  let zoneslist = Object.keys(ct.getAllTimezones());
+
+  for (let zone in zoneslist) {
+    let timezoneName = zoneslist[zone];
+    if (timezoneName == 'Europe/Kiev') {
+      timezoneName = 'Europe/Kyiv';
+    }
+
+    let timezoneSlug = timezoneName.replace(/\//g, '-');
+
+    items.push({
+      url: `https://${config.url}/timezone/${timezoneSlug}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.4,
+      alternates: createAlternates(`/timezone/${timezoneSlug}`),
     });
   }
 
