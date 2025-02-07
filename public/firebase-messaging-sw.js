e339c383acfea52ca90029ae29633ac4b42fd939
ay8s <contact@andydev.co.uk>
2025-02-07 15:46:35 -0800

    Set values again

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,13 +5,14 @@ importScripts(
   'https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js',
 );
 
+// https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public/37484053#37484053
 firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyD-gSAj27EYbYc4eR_DtMzJ_BgnGreT414',
+  authDomain: 'f1-calendar-678ba.firebaseapp.com',
+  projectId: 'f1-calendar-678ba',
+  storageBucket: 'f1-calendar-678ba.firebasestorage.app',
+  messagingSenderId: '1086945121867',
+  appId: '1:1086945121867:web:778ae86f34cfce99c95fce',
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
