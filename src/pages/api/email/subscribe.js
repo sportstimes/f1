c230f8eef7f802ba64ae972f19f50c5187742e8c
ay8s <contact@andydev.co.uk>
2025-02-25 17:19:51 -0800

    Add new subscribe code

--- src/pages/api/email/subscribe.js
+++ src/pages/api/email/subscribe.js
@@ -1,35 +1,41 @@
-import { Novu } from '@novu/node';
+import admin from 'firebase-admin';
 
 export default async (req, res) => {
-	if (!req.body.identifier) {
-		return res.status(400).json({
-			success: false,
-			message: "No identifier defined."
-		});
-	}
-	
-	if (!req.body.email) {
-		return res.status(400).json({
-			success: false,
-			message: "No email defined."
-		});
-	}
-	
-	const config = await import(`/_db/${process.env.NEXT_PUBLIC_SITE_KEY}/config.json`)  
-	
-	let email = req.body.email;
-	
-	// Prefix the topic with the site key so we can support reminders other sites also.
-	let topic = `${process.env.NEXT_PUBLIC_SITE_KEY}-reminder`;
-	
-	const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API);
-	const subscriptionResponse = await novu.subscribers.identify(req.body.identifier, {
-		email: email,
-	})
-	
-	const response = await novu.topics.addSubscribers(topic, {
-		subscribers: [req.body.identifier],
-	});
-	
-	return res.json({success:true});
-}
+  if (!req.body.identifier) {
+    return res.status(400).json({
+      success: false,
+      message: 'No identifier defined.',
+    });
+  }
+
+  if (!req.body.email) {
+    return res.status(400).json({
+      success: false,
+      message: 'No email defined.',
+    });
+  }
+
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(
+        JSON.parse(process.env.FIREBASE_CREDENTIALS),
+      ),
+    });
+  }
+  const db = admin.firestore();
+
+  let email = req.body.email;
+
+  try {
+    // Add a document with an auto-generated ID
+    const docRef = await db
+      .collection(`${process.env.NEXT_PUBLIC_SITE_KEY}-subscriptions`)
+      .add({
+        email: email,
+      });
+
+    return res.json({ success: true });
+  } catch (error) {
+    return res.json({ success: false });
+  }
+};
